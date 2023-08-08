a6650a8b055117139776daabc66f167d6a5a9daa
lunkov <89242194144@mail.ru>
2023-08-08 22:47:02 +1000

    database added

--- handlers/admin.py
+++ handlers/admin.py
@@ -4,6 +4,9 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
 
 from create_bot import dp, bot
+from data_base import sqlite_db
+from keyboards import admin_kb
+
 
 ID = None
 
@@ -20,7 +23,7 @@ class FSMAdmin(StatesGroup):
 async def make_changes_command(message: types.Message):
     global ID
     ID = message.from_user.id
-    await bot.send_message(message.from_user.id, 'Что хозяин надо!?')
+    await bot.send_message(message.from_user.id, 'Что хозяин надо!?', reply_markup=admin_kb.button_case_admin)
     await message.delete()
 
 
@@ -30,6 +33,18 @@ async def cm_start(message : types.Message):
     if message.from_user.id == ID:
         await FSMAdmin.photo.set()
         await message.reply('Загрузи фото')
+
+
+# Выход из состояний. Для любого машинного состояния
+# state="*", вводя команду "отмена", в любом регистре
+# @dp.message_handler(state="*", commands='отмена')  # Почему-то именно двойные кавычки
+# @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Загрузка прекращена')
 
 
 # Ловим первый ответ и пишем в словарь
@@ -67,30 +82,17 @@ async def load_price(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['price'] = float(message.text)
 
-    async with state.proxy() as data:
-        await message.reply(str(data))
+    await sqlite_db.sql_add_command(state)
     await state.finish()
-
-
-# Выход из состояний
-# Для любого машинного состояния state="*", вводя команду "отмена",
-# В любом регистре
-# @dp.message_handler(state="*", commands='отмена')  # Почему-то именно двойные кавычки
-# @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('Загрузка прекращена')
 
 
 def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
+    dp.register_message_handler(make_changes_command, commands=['модератор'], is_chat_admin=True)
     dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state="*", commands='отмена')
-    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
-    dp.register_message_handler(make_changes_command, commands=['модератор'], is_chat_admin=True)
+
