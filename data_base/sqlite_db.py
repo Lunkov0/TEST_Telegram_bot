2e0a35213a741b9073b34efa54eb742a9b84c230
lunkov <89242194144@mail.ru>
2023-08-12 15:08:43 +1000

    Added reading from database

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,5 +1,6 @@
 import sqlite3 as sq
 
+from create_bot import bot
 
 def sql_start():
     global base, cur
@@ -16,3 +17,25 @@ async def sql_add_command(state):
     async with state.proxy() as data:
         cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
         base.commit()
+
+
+async def sql_read(message):
+    for ret in cur.execute('SELECT * FROM menu').fetchall():
+        await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена: {ret[-1]}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- handlers/client.py
+++ handlers/client.py
@@ -1,5 +1,6 @@
 from aiogram import types, Dispatcher
 from create_bot import bot, dp
+from data_base import sqlite_db
 from keyboards import kb_client
 from aiogram.types import ReplyKeyboardRemove
 
@@ -12,7 +13,35 @@ async def command_start(message : types.message):
     except:
         await message.reply('Общение с ботом через личные сообщения: \nhttps://t.me/lunkovBot')
 
+
+# Выгружаем данные из базы
+@dp.message_handler(commands=['Меню'])
+async def pizza_menu_command(message: types.Message):
+    await sqlite_db.sql_read(message)
+
+
 # Таким образом мы пробрасываем функции в файл bot_telegram
-def register_handlers_client(dp : Dispatcher):
+def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
+    dp.register_message_handler(pizza_menu_command, commands=['Меню'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
